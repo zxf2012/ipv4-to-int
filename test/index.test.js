5ab6a9423c1333634bbb51bd7a8c30eb1daddce7
eric <eric@erics-MacBook-Pro.local>
2018-06-11 21:53:51 +0800

    add test case

--- test/index.test.js
+++ test/index.test.js
@@ -13,6 +13,11 @@ describe('test index', function() {
     it('should "172 .  168.5.1" equal to 2896692481', function() {
         assert.equal(toInt('172 .  168.5.1'), 2896692481);
     })
+    it('should "1 72.168.5.1" throw error', function() {
+        assert.throws(function () {
+            toInt('1 72.168.5.1')
+        }, /^Error: error ipv4$/)
+    })
     it('should " 172.168.5.1" throw error', function() {
         assert.throws(function () {
             toInt(' 172.168.5.1')
